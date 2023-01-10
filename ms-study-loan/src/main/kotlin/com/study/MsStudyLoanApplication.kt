e1074977cdcff43efb169ced5dd0c002eedf8fa4
Luiz Calazans <luizfernando.calazans8@gmail.com>
2023-01-09 22:46:51 -0300

    Add ms-study-loan project

---
+++ ms-study-loan/src/main/kotlin/com/study/MsStudyLoanApplication.kt
@@ -0,0 +1,11 @@
+package com.study
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsStudyLoanApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsStudyLoanApplication>(*args)
+}
